4e7e8898d4ef6542830263d2ea859ddf4b282db8
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-04-20 22:25:47 +0300

    card component created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(contact) {
+  return <Card 
+        key={contact.id}
+        name= {contact.name}
+        imgSrc = {contact.imgURL}
+        phone = {contact.phone}
+        email = {contact.email}
+  />
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card />
+      {contacts.map(createCard)}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
-function Card() {
+function Card(props) {
     return <div className="card">
         <div className="top">
-          <h2 className="name">Beyonce</h2>
+          <h2 className="name">{props.name}</h2>
           <img
             className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+            src={props.imgSrc}
             alt="avatar_img"
           />
         </div>
         <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div>
 }
